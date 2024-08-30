b939c28ff341cbdcd56b9fa8bbfbe226e4894748
Subin Kumar <158267844+hacragon@users.noreply.github.com>
2024-08-31 03:05:58 +0530

    Update crtsh-script.sh

--- crtsh-script.sh
+++ crtsh-script.sh
@@ -1,10 +1,7 @@
 curl -sk "https://crt.sh/?q=olacabs.com" | tee crt.txt
 
-
 cat crt.txt | grep '<TD>' | grep -vE '<TD>olacabs.com | <TD><A style = '|sed 's/<TD>//g' | sed 's/<\/TD>//g' | tee subdomain-crtsh.txt
-
 
 sed 's/^ *\*\.*/\n/g; s/^ *//; s/<BR>/\n/g' subdomain-crtsh.txt > crtsh-subdomain.txt 
 
-
- curl -s "https://crt.sh/?q=airbnb.com" | grep -oP '\b[\w.-]+\.airbnb\.com\b|\bairbnb\.com\.\w+\b' | tee airbnb-crt.txt
+curl -s "https://crt.sh/?q=airbnb.com" | grep -oP '\b[\w.-]+\.airbnb\.com\b|\bairbnb\.com\.\w+\b' | tee airbnb-crt.txt
